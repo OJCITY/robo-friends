e53f16115ee8e92a0c87b45461529133e555be8e
SEGUN OJO <ojcity_22@yahoo.com>
2022-03-17 17:08:14 +0100

    Updates

---
+++ precache-manifest.1f88e54234522c3665e365368bc8a541.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "e70b2131e82fed8d4e21",
+    "url": "/robo-friends/static/css/main.a9c1cd40.chunk.css"
+  },
+  {
+    "revision": "e70b2131e82fed8d4e21",
+    "url": "/robo-friends/static/js/main.e70b2131.chunk.js"
+  },
+  {
+    "revision": "7ab2393a9ad8c2b58dcd",
+    "url": "/robo-friends/static/js/1.7ab2393a.chunk.js"
+  },
+  {
+    "revision": "8bfa530e192b82bf9d5b",
+    "url": "/robo-friends/static/css/2.996838ba.chunk.css"
+  },
+  {
+    "revision": "8bfa530e192b82bf9d5b",
+    "url": "/robo-friends/static/js/2.8bfa530e.chunk.js"
+  },
+  {
+    "revision": "cae71618bc9424ae052b",
+    "url": "/robo-friends/static/js/runtime~main.cae71618.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robo-friends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "456a66995b4cc46dc198268e0a96d94c",
+    "url": "/robo-friends/index.html"
+  }
+];
